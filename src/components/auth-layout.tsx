b3c30211c5e6be5a8ea3c0bffa34d03c6d87f386
Dudu Freitas <dudufreitao08@gmail.com>
2025-10-23 21:01:05 +0000

    retire  (_for element <Logo>_)

--- src/components/auth-layout.tsx
+++ src/components/auth-layout.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from 'react';
 import Link from 'next/link';
-import { Logo } from '@/components/icons/logo';
 
 interface AuthLayoutProps {
   children: ReactNode;
@@ -12,7 +11,6 @@ export function AuthLayout({ children }: AuthLayoutProps) {
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-background bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(119,141,169,0.3),rgba(255,255,255,0))]"></div>
       <div className="mb-8 flex flex-col items-center">
         <Link href="/" className="flex items-center gap-2">
-          <Logo className="h-8 w-8 text-primary" />
           <h1 className="text-2xl font-bold tracking-tight text-foreground">
             🌙 Código Do Sono
           </h1>
