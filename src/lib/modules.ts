2d607cf246c88b0b6df3624d527c62ba9cd66b40
Dudu Freitas <dudufreitao08@gmail.com>
2025-10-23 22:42:56 +0000

    troque esta imagem por: https://imgur.com/a/BbBv5jC.png (_for element <C

--- src/lib/modules.ts
+++ src/lib/modules.ts
@@ -6,7 +6,7 @@ export const modules: Module[] = [
     id: 'module-1',
     title: 'Introdução',
     icon: BookOpen,
-    imageUrl: 'https://i.imgur.com/LCooIWw.png',
+    imageUrl: 'https://i.imgur.com/BbBv5jC.png',
   },
   {
     id: 'module-2',
