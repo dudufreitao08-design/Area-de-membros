55580b8bf82e714451cb39fad5d6a4b645a0a70b
Dudu Freitas <dudufreitao08@gmail.com>
2025-10-25 17:19:50 +0000

    Substitua, independente se mudar o tamanho do card, a imagem do módulo 1

--- src/lib/modules.ts
+++ src/lib/modules.ts
@@ -6,7 +6,7 @@ export const modules: Module[] = [
     id: 'module-1',
     title: 'Introdução',
     icon: BookOpen,
-    imageUrl: 'https://i.imgur.com/5LBLDT3.png',
+    imageUrl: 'https://i.imgur.com/5LBLDT3.jpg',
   },
   {
     id: 'module-2',
